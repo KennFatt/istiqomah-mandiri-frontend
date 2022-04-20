7e5e441f900e216d92ab8b239da9b723e49084a5
Kennan Fattahillah <kennfatt@gmail.com>
2022-04-20 22:52:56 +0700

    feat: add global fonts

---
+++ src/pages/_document.tsx
@@ -0,0 +1,24 @@
+import Document, { Head, Html, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          {/* Google Fonts */}
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Mulish:wght@400;500;600;700&display=optional"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
---
+++ src/utils/chakra-theme.ts
@@ -0,0 +1,8 @@
+import { extendTheme } from "@chakra-ui/react";
+
+export const theme = extendTheme({
+  fonts: {
+    heading: "Mulish, sans-serif",
+    body: "Mulish, sans-serif",
+  },
+});
