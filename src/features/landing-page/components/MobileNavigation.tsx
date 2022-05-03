b8b1b15bff94001b4cc3bfa47f8153e827c85c72
Kennan Fattahillah <kennfatt@gmail.com>
2022-05-03 18:20:32 +0700

    feat(`feature/user-interface`): impl mobile navigation drawer

--- src/features/landing-page/components/MobileNavigation.tsx
+++ src/features/landing-page/components/MobileNavigation.tsx
@@ -4,17 +4,28 @@ import {
   DrawerContent,
   DrawerOverlay,
   IconButton,
-  Text,
   useDisclosure,
+  VStack,
+  Link as ChakraLink,
+  useMediaQuery,
 } from "@chakra-ui/react";
 import { FC, useRef } from "react";
 import { BaseProps } from "utils/react-fc-props";
+import Link from "next/link";
+import { NAVIGATION_LIST } from "../navigation-list";
 
 interface MobileNavigationProps extends BaseProps {}
 
+/**
+ * @private
+ */
 export const MobileNavigation: FC<MobileNavigationProps> = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const buttonRef = useRef<HTMLButtonElement | null>(null);
+
+  const [isOnDesktopBreakpoint] = useMediaQuery("(min-width: 992px)");
+
+  const shouldShowDrawer = !isOnDesktopBreakpoint ? isOpen : false;
 
   return (
     <>
@@ -28,13 +39,37 @@ export const MobileNavigation: FC<MobileNavigationProps> = () => {
       />
 
       <Drawer
-        isOpen={isOpen}
+        isOpen={shouldShowDrawer}
         onClose={onClose}
         finalFocusRef={buttonRef}
         placement="right"
       >
         <DrawerOverlay />
-        <DrawerContent>{/* TODO */}</DrawerContent>
+        <DrawerContent>
+          <VStack p="2">
+            {NAVIGATION_LIST.map((navItem, index) => (
+              <Link key={index} href={navItem.href} passHref>
+                <ChakraLink
+                  p="2"
+                  w="full"
+                  color="gray.600"
+                  fontWeight="medium"
+                  display="block"
+                  textAlign="center"
+                  borderRadius="md"
+                  _hover={{
+                    cursor: "pointer",
+                    color: "gray.700",
+                    transition: "color",
+                    transitionDuration: "500ms",
+                  }}
+                >
+                  {navItem.name}
+                </ChakraLink>
+              </Link>
+            ))}
+          </VStack>
+        </DrawerContent>
       </Drawer>
     </>
   );
