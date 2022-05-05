042a8f8a04694ff4dc70bd0419804302183e5766
Kennan Fattahillah <kennfatt@gmail.com>
2022-05-05 21:38:52 +0700

    feat(`feature/user-interface`): add new About section.
    - TODO: refactor into its own component (and probably make the layout reusable)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,88 @@
-import { Hero, InfaqFinder, LandingPageLayout } from "features/landing-page";
+import {
+  Center,
+  Stack,
+  Text,
+  useBreakpointValue,
+  VStack,
+} from "@chakra-ui/react";
+import {
+  Hero,
+  InfaqFinder,
+  LandingPageLayout,
+  SectionContainer,
+} from "features/landing-page";
 
 import type { NextPageWithLayout } from "utils/common-types";
 
 const Home: NextPageWithLayout = () => {
+  const subtitleFontSize = useBreakpointValue({
+    base: "xl",
+    lg: "2xl",
+  });
+
+  const titleFontSize = useBreakpointValue({
+    base: "2xl",
+    lg: "4xl",
+  });
+
   return (
     <>
       <Hero />
       <InfaqFinder />
+
+      <SectionContainer containerProps={{ bg: "white", py: "12", my: "8" }}>
+        <Stack direction={{ base: "column", sm: "row" }} spacing="4">
+          {/* LHS: Title */}
+          <Center w={{ base: "full", sm: "50%" }} justifyContent="start">
+            <VStack alignItems="start" spacing="2">
+              <Text
+                fontWeight="medium"
+                fontSize={subtitleFontSize}
+                color="gray.600"
+              >
+                Tentang
+              </Text>
+
+              <Text
+                fontWeight="semibold"
+                fontSize={titleFontSize}
+                color="green.500"
+              >
+                Apa Itu Istiqomah Mandiri?
+              </Text>
+            </VStack>
+          </Center>
+
+          {/* RHS: Content */}
+          <VStack
+            w={{ base: "full", sm: "50%" }}
+            color="gray.600"
+            spacing="5"
+            alignItems="start"
+          >
+            <Text>
+              Minim laborum magna et sunt velit aute. Id veniam est
+              reprehenderit commodo labore labore dolor elit. Irure exercitation
+              aute cillum minim aliquip cillum. Occaecat est deserunt incididunt
+              anim nulla sit quis elit id amet cillum officia dolore. Qui sunt
+              incididunt aute nisi ex exercitation fugiat laborum sint irure sit
+              nisi.
+            </Text>
+
+            <Text>
+              Duis nostrud amet aliquip ad. Adipisicing voluptate id do pariatur
+              reprehenderit est occaecat deserunt et officia adipisicing. Mollit
+              dolore est anim esse ipsum exercitation dolor non Lorem.
+            </Text>
+
+            <Text>
+              Nostrud magna adipisicing exercitation aliquip nulla laboris esse
+              proident. Consequat labore mollit ullamco ullamco. Est sit sint
+              esse enim aliqua.
+            </Text>
+          </VStack>
+        </Stack>
+      </SectionContainer>
     </>
   );
 };
